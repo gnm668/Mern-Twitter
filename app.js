334a1eea76b4acf9ceb38038620c6d108103b5e6
Al <nguyenvtalexander@gmail.com>
2019-11-18 09:10:48 -0800

    mern twitter

--- app.js
+++ app.js
@@ -2,11 +2,24 @@
 const express = require('express');
 //create app object
 const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+const passport = require('passport');
+
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('connected to mongoDB'))
+    .catch(err => console.log(err));
 
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');   
-});
+// You can now delete our 'Hello World' route
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
 
 const port = process.env.PORT || 5000;
 
